2758537b43803bfd21ecd6dfad6a1b93c5eb57c8
cristell calderon <calderoncristell948@gmail.com>
2026-03-16 18:45:21 -0500

    feat: sistema de ranking global con Firebase Auth

--- src/app/services/character.service.ts
+++ src/app/services/character.service.ts
@@ -1,25 +1,32 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Character } from '../character.model';
-import { Observable } from 'rxjs';
+import { Observable, forkJoin } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CharacterService {
-  private apiUrl = 'https://rickandmorty-backend-production-7746.up.railway.app/characters';
+
+  private apiUrl = 'https://rickandmortyapi.com/api/character';
 
   constructor(private http: HttpClient) {}
 
   getCharacters(): Observable<Character[]> {
-    return this.http.get<Character[]>(this.apiUrl);
+    const pages = [1, 2, 3];
+    return forkJoin(
+      pages.map(p => this.http.get<any>(`${this.apiUrl}?page=${p}`))
+    ).pipe(
+      map(results => results.flatMap((r: any) => r.results))
+    );
   }
 
   getCharacterById(id: number): Observable<Character> {
-  return this.http.get<Character>(`${this.apiUrl}/${id}`);
-}
+    return this.http.get<Character>(`${this.apiUrl}/${id}`);
+  }
+
   getEpisodeNameByUrl(url: string): Observable<any> {
     return this.http.get<any>(url);
   }
-
 }
